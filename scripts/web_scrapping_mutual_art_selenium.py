460ecb7cf0427d3ed8c35956ef6e729b1ca3b006
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2022-09-22 08:47:50 -0500

    web scraping con selenium

--- scripts/web_scrapping_mutual_art_selenium.py
+++ scripts/web_scrapping_mutual_art_selenium.py
@@ -1,11 +1,18 @@
 #! /Users/mariano/anaconda3/bin/python
 
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+# Abrimos los códigos de las nacionalidades latinoamericanas
+nacionalidades = pd.read_csv('../datasets/xpathsPaises.csv', header=None)
+paises = nacionalidades[0].values
+nacionalidades = nacionalidades[1].values
+
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 driver.maximize_window()
@@ -21,11 +28,18 @@ email.send_keys('jmarinms@hotmail.com')
 password.send_keys('Foncarte19')
 password.send_keys(Keys.RETURN)
 
-time.sleep(2)
+time.sleep(4)
+# Entramos en las auctions
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Auctions'))).click()
+# Elegimos
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[1]/div/nav/ul/li[3]/a'))).click()
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[2]/div/div/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/ul/li[1]/label/span[2]'))).click()
-
+# Elegimos subastas que ya se hayan concretado (por el momento lo he deshabilitado)
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[2]/div/div/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/ul/li[1]/label/span[2]'))).click()
+# Elegimos solo artistas latinoamericanos
+for pais, nacionalidad in zip(paises, nacionalidades):
+    time.sleep(3)
+    print(pais)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, nacionalidad))).click()
 
 for i in range(100000):
     time.sleep(5)
